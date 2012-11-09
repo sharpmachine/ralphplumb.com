f4002acc436e50b13bebec05878fb77c957f9ffd
Jesse Kade <jessekade@Sharp-Machine.local>
2012-11-08 17:52:42 -0800

    initial commit

---
+++ wp-content/themes/ralphplumb/home.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<section id="page">
+	<div class="row" id="book">
+		<div class="span5" id="form">
+			<ul class="unstyled">
+				<li>Receive more leads through LinkedIn Groups</li>
+				<li>Become a thought leader with LinkedIn SEO Tips</li>
+				<li>Attract hundred of targeted attendees to events</li>
+			</ul>
+			<form action="" class="signup">
+				<input type="text" placeholder="Your Name">
+				<input type="email" placeholder="Your Email"><br>
+				<input type="submit" value="Download Now">
+			</form>
+		</div>
+		<div class="span5 offset1" id="blog">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis assumenda deserunt eius nulla vel ea reiciendis nam. Eos molestiae nemo assumenda neque eaque. Nemo eos dolor nihil quisquam aliquid aperiam!</p>
+		</div>
+
+	</div>
+	
+</section><!-- #page -->
+
+<?php get_footer(); ?>
