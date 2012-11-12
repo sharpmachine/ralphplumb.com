c6611a0d3d85a5ecbccaa8afe6df98359f2c8f88
Jesse Kade <jessekade@Sharp-Machine.local>
2012-11-12 11:25:52 -0800

    removed unneeded files

--- wp-content/themes/ralphplumb/footer.php
+++ wp-content/themes/ralphplumb/footer.php
@@ -11,14 +11,9 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-
-
-		<!-- scripts concatenated and minified via ant build script-->
 		<script src="<?php bloginfo ('template_directory'); ?>/js/plugins-ck.js"></script>
 		<script src="<?php bloginfo ('template_directory'); ?>/js/script-ck.js"></script>
-		<!-- Remove these before deploying to production -->
-		<script src="<?php bloginfo ('template_directory'); ?>/js/hashgrid.js" type="text/javascript"></script>
-
+		
 		<?php wp_footer(); ?>
 	</body>
 </html>
